ce69a4711fa44522c723a06ca68f9c1f76a592d1
nmasters <nmasters52@gmail.com>
2025-12-16 11:02:35 -0500

    tailwind config set up

---
+++ data.js
@@ -0,0 +1,21 @@
+// export const CATEGORIES = [
+//   "mortar-shells",
+//   "salute-mortar-shells",
+//   "cakes",
+//   "salute-cakes",
+//   "big-bores",
+//   "roman-candles",
+//   "rockets",
+//   "misc"
+// ];
+
+// export const CATEGORY_LABEL = {
+//   "mortar-shells": "Mortar Shells",
+//   "salute-mortar-shells": "Salute Mortar Shells",
+//   cakes: "Cakes",
+//   "salute-cakes": "Salute Cakes",
+//   "big-bores": "Big Bores",
+//   "roman-candles": "Roman Candles",
+//   rockets: "Rockets",
+//   misc: "Misc"
+// };
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import React from "react";
 import { Hero } from "./components/Hero";
-
-// export const CATEGORIES = [
-//   "mortar-shells",
-//   "salute-mortar-shells",
-//   "cakes",
-//   "salute-cakes",
-//   "big-bores",
-//   "roman-candles",
-//   "rockets",
-//   "misc"
-// ];
-
-// export const CATEGORY_LABEL = {
-//   "mortar-shells": "Mortar Shells",
-//   "salute-mortar-shells": "Salute Mortar Shells",
-//   cakes: "Cakes",
-//   "salute-cakes": "Salute Cakes",
-//   "big-bores": "Big Bores",
-//   "roman-candles": "Roman Candles",
-//   rockets: "Rockets",
-//   misc: "Misc"
-// };
 
 const App = () => {
   return (
-    <div className="text-red-500 bg-gray-700 h-screen">
+    <div className="w-auto bg-background">
       <Hero />
+      <div className="max-w-3xl m-auto flex justify-center p-4 "></div>
     </div>
   );
 };
